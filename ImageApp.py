1638fa0a956b84a007d5e1f0d2aaf41434b10b3a
Dylan Bowman <dbowman06@gmail.com>
2018-01-05 16:40:56 -0800

    Creates a carousel.

---
+++ ImageApp.py
@@ -0,0 +1,6 @@
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.core.image import Image
+
+class ImageApp(RelativeLayout):
+	def __init__(self,**kwargs):
+		super(ImageApp, self).__init__(**kwargs)
--- main.py
+++ main.py
@@ -1,110 +1,25 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.config import Config
+from kivy.uix.carousel import Carousel
+from kivy.uix.image import AsyncImage
+from kivy.clock import Clock
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.clock import Clock
 from kivy.properties import StringProperty
-from kivy.cache import Cache
-from kivy.config import Config
-from kivy.core.image import Image
 
-import time,json
+import time
 
-import weather
+import ImageApp, TimeZoneApp
 
-class Slide():
-	def __init__(self, filename="./images/mountain.jpg", headline="Headline", caption="This is where a caption would go."):
-		self.filename = filename
-		self.headline = headline
-		self.caption = caption
-	def __str__(self):
-            return("Filename: "+self.filename+"\nHeadline: " +self.headline + "\nCaption: "+self.caption)
 
 class WorkTV(RelativeLayout):
-
-	ImageSource = StringProperty()
-	Headline = StringProperty()
-	Caption = StringProperty()
-
-	slideIndex = 0
-	currentSlide = Slide()
-
-	slideTime = 10
-	
-
-
-	def __init__(self, **kwargs):
+	def __init__(self,**kwargs):
 		super(WorkTV, self).__init__(**kwargs)
-		print("Initializing")
-		fileHandle = open("./config.json")
-		contents = fileHandle.read()
-		fileHandle.close()
-		try:
-                    jsonData = json.loads(contents)
-                except:
-                    print("Something went wrong, dumping contents:")
-                    print(contents)
-                    quit()
-		outList = []
-		for d in jsonData["slides"]:
-			s = Slide(d["img"],d["head"],d["desc"])
-			outList.append(s)
-		self.settings = jsonData["settings"]
-		self.slideTime = self.settings["time"]
-		self.weatherTime = self.settings["weather-update"]
-		self.weatherLocation = self.settings["weather-location"]
-		self.slides = outList
-		
-		print("Slides loaded:")
-		for slide in self.slides:
-                    print(slide)
 
-		self.currentSlide = self.slides[self.slideIndex]
-		self.ImageSource = self.currentSlide.filename
-		self.updated = True
-		self.oldTime = 0;
-		self.runTime = 0;
-		
-
-	
-
-	def setContainer(self, cont):
-		self.container = cont;
-
-	def update(self, *args):
-		t = int(time.time())
-		if t != self.oldTime:
-			self.runTime += 1
-			if self.runTime % self.slideTime == 0:
-                                
-				self.slideIndex = (self.slideIndex + 1) % len(self.slides)
-				self.currentSlide = self.slides[self.slideIndex]
-				self.ImageSource = self.currentSlide.filename
-				#print("Changed slide:")
-				#print(self.currentSlide)
-				#print("Read file: "+self.ImageSource)
-			self.oldTime = t
-
-		for child in self.children[0].children:
-			updateFN = getattr(child, "update", None)
-			if callable(updateFN):
-				child.update(self.currentSlide, self.settings, self.runTime)
-
-
-
-	def close(self, *args):
-		for child in self.children[0].children:
-			closeFN = getattr(child, "close", None)
-			if callable(closeFN):
-				child.close()	
-
-class BottomRibbon(RelativeLayout):
-	def __init__(self, **kwargs):
-		super(BottomRibbon, self).__init__(**kwargs)
-		self.slide=Slide()
+		self.ids.appContainer.setCaptionBox(self.ids.bottomRibbon.ids.captionBox)
 
 	def update(self, *args):
 		for child in self.children[0].children:
@@ -112,33 +27,73 @@ class BottomRibbon(RelativeLayout):
 			if callable(updateFN):
 				child.update(args)
 
-	def setSlide(slide):
-		self.slide = slide
+class BottomRibbon(RelativeLayout):
+	def __init__(self, **kwargs):
+		super(BottomRibbon, self).__init__(**kwargs)
+
+	def update(self, *args):
+		for child in self.children[0].children:
+			updateFN = getattr(child, "update", None)
+			if callable(updateFN):
+				child.update(args)
 
 class LogoBox(RelativeLayout):
 	def __init__(self, **kwargs):
 		super(LogoBox, self).__init__(**kwargs)
 
+	def update(self, *args):
+		for child in self.children[0].children:
+			updateFN = getattr(child, "update", None)
+			if callable(updateFN):
+				child.update(args)
+
 class CaptionBox(RelativeLayout):
-
-	Headline = StringProperty()
-	Caption = StringProperty()
-
-	Weather = StringProperty()
+	headline = StringProperty()
+	caption = StringProperty()
+	weather = StringProperty()
 
 	def __init__(self, **kwargs):
 		super(CaptionBox, self).__init__(**kwargs)
-		self.weatherString = "Loading weather..."
 
 	def update(self, *args):
-		self.Headline = args[0][0].headline
-		self.Caption = args[0][0].caption
+		for child in self.children[0].children:
+			updateFN = getattr(child, "update", None)
+			if callable(updateFN):
+				child.update(args)			
 
-		if args[0][2] % args[0][1]["weather-update"] == 1:
-			print("Updating weather")
-			self.weatherString = weather.getWeather(args[0][1]["weather-location"])
 
-		self.Weather = self.weatherString + " | " + time.strftime("%I:%M %p").lstrip("0")
+class AppContainer(RelativeLayout):
+	transitionTime = 5
+
+
+	def __init__(self, **kwargs):
+		super(AppContainer, self).__init__(**kwargs)
+		self.runTime = 0
+		self.oldTime = 0
+
+		self.captionBox = None
+
+	def setCaptionBox(self,captionBox):
+		self.captionBox = captionBox
+
+	def update(self, *args):
+		for child in self.ids.Carousel.slides:
+			updateFN = getattr(child, "update", None)
+			if callable(updateFN):
+				child.update(args)
+
+		if (self.captionBox != None):
+			slide = self.ids.Carousel.current_slide
+			self.captionBox.headline = slide.headline
+			self.captionBox.caption = slide.caption
+
+
+		if (int(time.time()) != self.oldTime):
+			self.oldTime = int(time.time())
+			self.runTime += 1
+
+			if self.runTime % self.transitionTime == 0:
+				self.ids.Carousel.load_next()	
 
 class WorkTVApp(App):
 	def build(self):
@@ -147,11 +102,8 @@ class WorkTVApp(App):
 		Config.set('graphics', 'fullscreen', 'true')
 		self.load_kv('WorkTV.kv')
 		self.appWindow = WorkTV()
-		self.appWindow.setContainer(self)
-		Clock.schedule_interval(self.appWindow.update, .5)
+		Clock.schedule_interval(self.appWindow.update, .2)
 		return self.appWindow
-	def on_stop(self):
-		self.appWindow.close()
 
 if __name__ == "__main__":
 	WorkTVApp().run()
