ad830f541c7f5c73178cdb201a1204419c0853e8
Dylan T Bowman <dbowman06@gmail.com>
2019-01-11 12:15:26 -0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -9,26 +9,47 @@ oldWeather = "Loading weather..."
 
 def getWeather(location):
 	global oldWeather
-	baseurl = "https://query.yahooapis.com/v1/public/yql?q="
-	query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+location+'")'
-	form = "&format=json"
-	response = jsonRequests.getResponse(baseurl+query+form)
-	if response.status:
-		jsonData = response.data
-		condition = ""
+	# Using Weather.gov's API.
+	
+	forecast_url =  "https://api.weather.gov/gridpoints/SGX/43,50/forecast"
+	observation_url = "https://api.weather.gov/stations/E2061/observations/latest"
+
+	observation_response = jsonRequests.getResponse(observation_url)
+	temp = 0
+	if observation_response.status:
 		try:
-			condition = jsonData["query"]["results"]["channel"]["item"]["condition"]
-		except:
+			jsonData = observation_response.data
+			temp = celsiusToFahrenheit(jsonData["properties"]["temperature"]["value"])
+		except Exception as e:
+			print(e)
 			oldWeather = "Could not get weather."
-			return oldWeather	
-		oldWeather = condition["temp"] + " F | " + condition["text"]
-	else:
-		Logger.error("Weather Module: Had issues: "+response.message)
+			return oldWeather
+
+	forecast_response = jsonRequests.getResponse(forecast_url)
+	high = 0
+	low = 0
+	if forecast_response.status:
+		try:
+			jsonData = forecast_response.data
+			if jsonData["properties"]["periods"][0]["isDaytime"]:
+				high = jsonData["properties"]["periods"][0]["temperature"]
+				low = jsonData["properties"]["periods"][1]["temperature"]
+			else:
+				high = jsonData["properties"]["periods"][1]["temperature"]
+				low = jsonData["properties"]["periods"][2]["temperature"]
+		except Exception as e:
+			print(e)
+			oldWeather = "Could not get weather."
+
+			return oldWeather
+
+	oldWeather = str(int(round(temp))) + " F | " + str(high) + " / " + str(low)
 	return oldWeather
 
 forecast = ""
 condition = ""
 
+def celsiusToFahrenheit(celsius): return (9 * celsius / 5) + 32
 def getWeather_app(location):
 	return None, None
 	baseurl = "https://query.yahooapis.com/v1/public/yql?q="
